39716824b40301d829edb065c0ed84fe54d49482
sergeyplechko <sergeyplechko@gmail.com>
2025-05-12 18:41:09 +0300

    updated

--- src/test/java/guru/qa/ShopKixBoxPageNewArrivalsTest.java
+++ src/test/java/guru/qa/ShopKixBoxPageNewArrivalsTest.java
@@ -2,6 +2,7 @@ package guru.qa;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
@@ -15,6 +16,7 @@ import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverConditions.url;
 
+@DisplayName("Тестирование интернет-магазина KixBox")
 public class ShopKixBoxPageNewArrivalsTest {
 
     @BeforeAll
@@ -28,6 +30,7 @@ public class ShopKixBoxPageNewArrivalsTest {
     })
     @ParameterizedTest(name = "На странице отображается заголовок {0}")
     @Tag("MAJOR")
+    @DisplayName("На странице отображается заголовок 'Новинки'")
     void displayHeaderNewArrivalsPageTest(String expectedTitle) {
         open("/collection/new-arrivals");
         $("h1.page-headding").shouldBe(visible).shouldHave(text(expectedTitle));
@@ -37,6 +40,7 @@ public class ShopKixBoxPageNewArrivalsTest {
     @CsvFileSource(resources = "/test_data/checkOpenNeededSection")
     @ParameterizedTest(name = "При открытии раздела {0} с категорией {1} отображается ссылка {2}")
     @Tag("MAJOR")
+    @DisplayName("Отображается корректный url в зависимости от раздела и категории")
     void CheckDisplayUrlTest(String gender, String category, String url) {
 
         actions()
@@ -63,6 +67,7 @@ public class ShopKixBoxPageNewArrivalsTest {
     @ParameterizedTest
     @MethodSource("kixBoxHasCorrectSectionsMainPageFooterTest")
     @Tag("MAJOR")
+    @DisplayName("В футере есть разделы О нас, Помощь покупателю и Социальные сети")
     void kixBoxHasCorrectSectionsMainPageFooterTest(List<String> expectedSections) {
         open("https://kixbox.ru/");
         $$("div.menu-title").shouldHave(exactTexts(expectedSections));
