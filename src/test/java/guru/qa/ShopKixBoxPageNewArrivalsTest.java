27df9c5cc005007b0cb7443bdfd92961afdb7834
sergeyplechko <sergeyplechko@gmail.com>
2025-05-12 16:00:21 +0300

    updated

--- src/test/java/guru/qa/ShopKixBoxPageNewArrivalsTest.java
+++ src/test/java/guru/qa/ShopKixBoxPageNewArrivalsTest.java
@@ -1,28 +1,50 @@
 package guru.qa;
 
+import com.codeborne.selenide.selector.ByText;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverConditions.url;
+import static com.codeborne.selenide.files.DownloadActions.click;
 
-public class ShopKixBoxPageNewArrivalsTest extends Testbase {
+public class ShopKixBoxPageNewArrivalsTest {
+
+    @BeforeAll
+    static void actionsBeforeTestingNewArrivalsPage() {
+        open("https://kixbox.ru/collection/new-arrivals");
+    }
 
     @ValueSource(strings = {
-            "selenide", "JUnit 5"
+            "Новинки"
     })
     // Параметризованный тест - это такой тест, в котором тест-кейсы идентичны, но входные тестовые данные отличаются
-    @ParameterizedTest(name = "Для поискового запроса {0} должен отдавать не пустой список фото")
-    @Tag("BLOCKER")
-    void DropdownListsExistPageNewArrivalsTest(String dropDownlist) {
-
-        $(".filter__items").shouldBe(visible);
-        $("#header").find(byText("Images")).click();
-        $$(".oc9XKvSI3VFua2OmmSXU").shouldBe(sizeGreaterThan(0));
+    @ParameterizedTest(name = "На странице отображается заголовок {0}")
+    @Tag("MAJOR")
+    void headerNewArrivalsPageTest(String expectedTitle) {
+        $(".layout__content").shouldHave(text(expectedTitle));
     }
+
+
+
+@CsvFileSource(resources = "/test_data/checkOpenNeededSection")
+@ParameterizedTest(name = "При открытии раздела {0} с категорией {1} отображается ссылка {2}")
+@Tag("MAJOR")
+void CheckOpeningPageDependingCategoryAndGenderTest(String gender, String category, String url) {
+
+    actions().moveToElement($("nav.header-part-menu").find(byText(gender)))
+            .pause(400)
+            .perform();
+    actions().moveToElement($("nav.header-part-menu").find(byText(category))).click().perform();
+    webdriver().shouldHave(url(url));
+    actions().pause(500).perform();
+
+
+    }
+
 }
+
